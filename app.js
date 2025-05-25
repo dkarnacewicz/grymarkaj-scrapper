099c5ee88305b6af73e28a380b52d2e51faaf0c2
Dawid Karnacewicz <dawid969696@gmail.com>
2025-05-25 13:30:24 +0200

    change args

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.get("/scrapper", async (request, response) => {
 
   const url = request.query.url;
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: "networkidle2" });
